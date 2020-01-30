66f3f79618bd2d0fe7ca49b21af7ce8e40808d19
MilosVulic <monty.green97@gmail.com>
2020-01-30 16:06:11 +0100

    -- design enhanced

--- app/src/main/java/com/example/movietracker/adapters/TvShowAdapter.kt
+++ app/src/main/java/com/example/movietracker/adapters/TvShowAdapter.kt
@@ -5,26 +5,24 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
-import com.example.movietracker.activities.ContentDetails
 import com.example.movietracker.R
+import com.example.movietracker.activities.ContentDetails
 import com.example.movietracker.config.GlideApp
 import com.example.movietracker.dao.tvshow.TvShow
-import com.example.movietracker.view_models.TvShowViewModel
 
-class TvShowAdapter(var tvShowsViewModel: TvShowViewModel) :
-    RecyclerView.Adapter<TvShowAdapter.TvShowHolder>() {
+class TvShowAdapter : RecyclerView.Adapter<TvShowAdapter.TvShowHolder>() {
 
     private var listTvShows: List<TvShow> = ArrayList()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TvShowHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.tvshow_row, parent, false)
+        val itemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.tvshow_row, parent, false)
         return TvShowHolder(itemView)
     }
 
@@ -35,10 +33,6 @@ class TvShowAdapter(var tvShowsViewModel: TvShowViewModel) :
         holder.textViewTvShowRelealse.text = currentTvShow.tvshowYear.toString()
         holder.textViewNumberOfSeassons.text = currentTvShow.numberOfSeassons.toString()
         displayImage(holder.itemView.context, holder.imageView, currentTvShow.tvshowUri)
-
-        holder.buttonDelete.setOnClickListener {
-            tvShowsViewModel.deleteTvShow(currentTvShow)
-        }
 
         holder.cardViewTvShow.setOnClickListener { v ->
             val intent = Intent(v.context, ContentDetails::class.java)
@@ -56,6 +50,10 @@ class TvShowAdapter(var tvShowsViewModel: TvShowViewModel) :
         notifyDataSetChanged()
     }
 
+    fun getData(): List<TvShow?>? {
+        return listTvShows
+    }
+
     private fun displayImage(context: Context, imageView: ImageView, imageUri: String?) {
         val uri = imageUri?.toUri()
         GlideApp.with(context)
@@ -64,21 +62,12 @@ class TvShowAdapter(var tvShowsViewModel: TvShowViewModel) :
             .into(imageView)
     }
 
-    inner class TvShowHolder(itemView: View) : RecyclerView.ViewHolder(itemView),
-        View.OnClickListener {
+    inner class TvShowHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var textViewTvShowName: TextView = itemView.findViewById(R.id.textViewTvShowName)
         var textViewDescription: TextView = itemView.findViewById(R.id.textViewTvShowDescription)
         var textViewTvShowRelealse: TextView = itemView.findViewById(R.id.textViewTvShowYear)
         var textViewNumberOfSeassons: TextView = itemView.findViewById(R.id.textViewNumberOfSeassons)
-        var buttonDelete: Button = itemView.findViewById(R.id.buttonDeleteTvShow)
         var imageView: ImageView = itemView.findViewById(R.id.imageViewTvShow)
-        var cardViewTvShow: CardView = itemView.findViewById(R.id.card_view_tv_show)
-
-        init {
-            itemView.setOnClickListener(this)
-        }
-
-        override fun onClick(v: View?) {
-        }
+        var cardViewTvShow: CardView = itemView.findViewById(R.id.card_view)
     }
 }
--- app/src/main/java/com/example/movietracker/fragments/TvShowsFragment.kt
+++ app/src/main/java/com/example/movietracker/fragments/TvShowsFragment.kt
@@ -8,10 +8,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.movietracker.R
 import com.example.movietracker.adapters.TvShowAdapter
 import com.example.movietracker.dao.tvshow.TvShow
+import com.example.movietracker.utils.SwipeToDeleteCallback
 import com.example.movietracker.view_models.TvShowViewModel
 import kotlinx.android.synthetic.main.fragment_tv_shows.view.*
 
@@ -19,6 +22,7 @@ import kotlinx.android.synthetic.main.fragment_tv_shows.view.*
 class TvShowsFragment : Fragment() {
 
     private lateinit var tvShowViewModel: TvShowViewModel
+    private lateinit var adapter: TvShowAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,9 +32,23 @@ class TvShowsFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_tv_shows, container, false)
         tvShowViewModel = ViewModelProviders.of(this).get(TvShowViewModel::class.java)
         view.tvShows_recycler_view.layoutManager = LinearLayoutManager(context)
-        val adapter = TvShowAdapter(tvShowViewModel)
+        adapter = TvShowAdapter()
         view.tvShows_recycler_view.adapter = adapter
         tvShowViewModel.getTvShows()?.observe(this, Observer<List<TvShow>> { t -> adapter.setTvShows(t!!) })
+        enableSwipeToDeleteAndUndo(view.tvShows_recycler_view)
         return view
+    }
+
+    private fun enableSwipeToDeleteAndUndo(recyclerView: RecyclerView) {
+        val swipeToDeleteCallback: SwipeToDeleteCallback =
+            object : SwipeToDeleteCallback(this.context!!) {
+                override fun onSwiped(viewHolder: RecyclerView.ViewHolder, i: Int) {
+                    val position = viewHolder.adapterPosition
+                    val item: TvShow = adapter.getData()?.get(position)!!
+                    tvShowViewModel.deleteTvShow(item)
+                }
+            }
+        val itemTouchhelper = ItemTouchHelper(swipeToDeleteCallback)
+        itemTouchhelper.attachToRecyclerView(recyclerView)
     }
 }
